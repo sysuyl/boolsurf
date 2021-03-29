b9b31b925f6a5863956d2cc548aff5a7c335d691
Marzia Riso <riso@di.uniroma1.it>
2021-03-29 18:48:43 +0200

    Update svg parser (translations)

--- scripts/svg_parser.py
+++ scripts/svg_parser.py
@@ -4,10 +4,11 @@ import sys
 import json
 import numpy as np
 from xml.dom import minidom
+import xml.etree.ElementTree as ET
 
 
 def parse(infile, outfile):
-    doc = minidom.parse(infile) 
+    doc = minidom.parse(infile)
     path_strings = [path.getAttribute('d') for path
                     in doc.getElementsByTagName('path')]
 
@@ -26,31 +27,49 @@ def parse(infile, outfile):
 
     doc.unlink()
 
+
 def create_json(infile, outfile):
-    doc = minidom.parse(infile) 
-    path_strings = [path.getAttribute('d') for path
-                    in doc.getElementsByTagName('path')]
-    doc.unlink()
+    root = ET.parse(infile).getroot()
+    data = {'points_in_screenspace': True, 'points': [], 'polygons': []}
 
-    data = {'points_in_screenspace': True, 'points': [], 'polygons' : []}
-    for path in path_strings:
-        polygon = []
-
+    def parse_path(path):
         points = path[1:-2].lower().split("l")
         points = [point.split(",") for point in points]
         points = [[float(x), float(y)] for x, y in points]
         points = points[:-1]
+        return points
 
-        for point in points:
-            polygon.append(len(data['points']))
-            data['points'].append(point)
-        
-        data['polygons'].append(polygon)
+    for element in root:
+        if element.tag == "{http://www.w3.org/2000/svg}g":
+
+            transform = element.attrib['transform'][7:-1].split(',')[-2:]
+            translation = (float(transform[0]), float(transform[1]))
+            # inserire anche altre trasformazioni
+
+            for child in element:
+                polygon = []
+                path_points = parse_path(child.attrib['d'])
+                for point in path_points:
+                    point[0] += translation[0]
+                    point[1] += translation[1]
+                    polygon.append(len(data['points']))
+                    data['points'].append(point)
+
+                data['polygons'].append(polygon)
+
+        elif element.tag == "{http://www.w3.org/2000/svg}path":
+            path_points = parse_path(element.attrib['d'])
+            polygon = []
+            for point in path_points:
+                polygon.append(len(data['points']))
+                data['points'].append(point)
+            data['polygons'].append(polygon)
 
     with open(outfile, "w") as out:
         json.dump(data, out, indent=2)
 
     print(data)
+
 
 if __name__ == "__main__":
     infile = sys.argv[1]
