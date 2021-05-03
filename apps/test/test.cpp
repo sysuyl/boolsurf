bd45391024186f0c10fbfc58c489262636bfb181
Giacomo Nazzaro <nazzarogiacomo@gmail.com>
2021-05-03 16:36:01 +0200

    update

--- apps/test/test.cpp
+++ apps/test/test.cpp
@@ -9,11 +9,8 @@
 #include "serialize/serialize.h"
 using namespace yocto;
 
-void save_image(const string& output_filename, const bool_mesh& mesh,
-    const bool_state& state, const scene_camera& camera, bool color_shapes,
-    int spp) {
-  if (output_filename == "no-output") return;
-
+scene_model make_scene(const bool_mesh& mesh, const bool_state& state,
+    const scene_camera& camera, bool color_shapes) {
   auto scene = scene_model{};
   scene.cameras.push_back(camera);
 
@@ -40,7 +37,7 @@ void save_image(const string& output_filename, const bool_mesh& mesh,
       instance.shape     = (int)scene.shapes.size();
       instance.material  = (int)scene.materials.size();
       auto& material     = scene.materials.emplace_back();
-      material.color     = {0, 0, 0};
+      material.color     = {0, 0, 1};
       material.type      = scene_material_type::glossy;
       material.roughness = 0.5;
       auto& edges        = scene.shapes.emplace_back();
@@ -107,30 +104,53 @@ void save_image(const string& output_filename, const bool_mesh& mesh,
       shape.lines       = lines;
     }
   }
+  return scene;
+}
+
+void save_image(const string& output_image_filename, const bool_mesh& mesh,
+    const bool_state& state, const scene_camera& camera, bool color_shapes,
+    int spp) {
+  if (output_image_filename == "no-output") return;
+
+  auto scene = make_scene(mesh, state, camera, color_shapes);
 
   auto params    = trace_params{};
   auto error     = string{};
   params.sampler = trace_sampler_type::eyelight;
   params.samples = spp;
   auto image     = trace_image(scene, params);
-  save_image(output_filename, image, error);
+  save_image(output_image_filename, image, error);
+}
+
+void save_image(
+    const string& output_image_filename, const scene_model& scene, int spp) {
+  if (output_image_filename == "no-output") return;
+  auto params    = trace_params{};
+  auto error     = string{};
+  params.sampler = trace_sampler_type::eyelight;
+  params.samples = spp;
+  auto image     = trace_image(scene, params);
+  save_image(output_image_filename, image, error);
 }
 
 int main(int num_args, const char* args[]) {
-  auto test_filename   = ""s;
-  auto output_filename = "data/render.png"s;
-  auto spp             = 4;
-  auto model_filename  = ""s;
-  auto svg_filename    = ""s;
-  auto svg_subdivs     = 2;
-  auto drawing_size    = 0.005f;
-  auto color_shapes    = false;
+  auto test_filename         = ""s;
+  auto output_image_filename = "data/render.png"s;
+  auto output_scene_filename = ""s;
+  auto spp                   = 4;
+  auto model_filename        = ""s;
+  auto svg_filename          = ""s;
+  auto svg_subdivs           = 2;
+  auto drawing_size          = 0.005f;
+  auto color_shapes          = false;
 
   // parse command line
   auto cli = make_cli("test", "test boolsurf algorithms");
   add_argument(
       cli, "input", test_filename, "Input test filename (.json).", {}, false);
-  add_option(cli, "output", output_filename, "Output image filename (.png).");
+  add_option(cli, "output_image", output_image_filename,
+      "Output image filename (.png).");
+  add_option(cli, "output_scene", output_scene_filename, "");
   add_option(cli, "spp", spp, "Samples per pixel.");
   add_option(cli, "model", model_filename, "Input model filename.");
   add_option(cli, "svg", svg_filename, "Input svg filename.");
@@ -189,8 +209,8 @@ int main(int num_args, const char* args[]) {
           test, mesh, bvh, test.camera, drawing_size, use_projection);
       printf("%s\n", "make_test_state");
 
-      // save_image(to_string(seed) + output_filename, mesh, state, test.camera,
-      // color_shapes, spp);
+      // save_image(to_string(seed) + output_image_filename, mesh, state,
+      // test.camera, color_shapes, spp);
 
       try {
         {
@@ -199,11 +219,11 @@ int main(int num_args, const char* args[]) {
         }
         compute_shapes(state);
 
-        save_image(
-            output_filename, mesh, state, test.camera, color_shapes, spp);
+        // save_image(
+        // output_image_filename, mesh, state, test.camera, color_shapes, spp);
 
-        auto graph_dir      = path_dirname(output_filename);
-        auto graph_filename = path_basename(output_filename) +
+        auto graph_dir      = path_dirname(output_image_filename);
+        auto graph_filename = path_basename(output_image_filename) +
                               string("_graph.png");
         auto graph_outfile = path_join(graph_dir, graph_filename);
         save_tree_png(state, graph_outfile, "", color_shapes);
@@ -243,12 +263,23 @@ int main(int num_args, const char* args[]) {
     state = state_from_test(mesh, test, 0.005, false);
     compute_cells(mesh, state);
     compute_shapes(state);
-    save_image(output_filename, mesh, state, test.camera, color_shapes, spp);
   }
 
-  auto graph_dir      = path_dirname(output_filename);
-  auto graph_filename = path_basename(output_filename) + string("_graph.png");
-  auto graph_outfile  = path_join(graph_dir, graph_filename);
+  auto scene = make_scene(mesh, state, test.camera, color_shapes);
+  if (output_scene_filename.size()) {
+    // for (auto& shape : scene.shapes) {
+    //   save_shape(shape, output_scene_filename + "/")
+
+    // }
+    save_scene(output_scene_filename, scene, error);
+  }
+
+  // save_image(output_image_filename, save_image, spp);
+
+  auto graph_dir      = path_dirname(output_image_filename);
+  auto graph_filename = path_basename(output_image_filename) +
+                        string("_graph.png");
+  auto graph_outfile = path_join(graph_dir, graph_filename);
 
   save_tree_png(state, graph_outfile.c_str(), "", color_shapes);
 
