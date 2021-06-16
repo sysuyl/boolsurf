6f1dcfbb9ee6e89ca6ded1c2514487cf8d0d2eb0
Marzia Riso <riso@di.uniroma1.it>
2021-06-16 17:55:38 +0200

    Single scene rendering

--- scripts/render.py
+++ scripts/render.py
@@ -15,7 +15,7 @@ def cli():
 @click.argument('bin')
 @click.argument('dirname')
 @click.argument('spp', default=1, type=int)
-def render(bin, dirname, spp):
+def render_all(bin, dirname, spp):
     scene_dirs = glob.glob(f'{dirname}/scenes/*')
     num_scenes = len(scene_dirs)
 
@@ -42,6 +42,26 @@ def render(bin, dirname, spp):
         print(cmd)
 
         subprocess.run(cmd, shell=True)
+
+
+@cli.command()
+@click.argument('bin')
+@click.argument('filename')
+@click.argument('spp', default=1, type=int)
+def render(bin, filename, spp):
+    dirname = os.path.dirname(filename)
+    name = os.path.basename(filename)
+
+    cmd = f'{bin} render {filename} --output {dirname}\\render.png --samples {spp}'
+    options = ' --envhidden --envname .\\data\\hdr-images\\doge2.hdr'
+    options += ' --denoise'
+    options += ' --exposure 1.0'
+    # options += ' --embreebvh'
+
+    cmd += options
+    print(cmd)
+
+    subprocess.run(cmd, shell=True)
 
 
 cli()
