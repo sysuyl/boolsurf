556de5bc5f6fc6f29456962ed0292b3f4e9e0f65
Marzia Riso <riso@di.uniroma1.it>
2021-07-12 16:54:44 +0200

    Added stats notebook

---
+++ scripts/convert-table.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def main():
+    csv_filename = sys.argv[1]
+    latex_table = []
+
+    with open(csv_filename) as stats:
+        lines = stats.readlines()
+
+        for i in range(1, len(lines)):
+            record_list = lines[i].strip().split(", ")
+            record = [record_list[0].replace(
+                "/", "\\").split("\\")[-1].split(".")[0]]
+
+            timings = [round(float(time), 3) for time in record_list[3:8]]
+            perc = [round((time/timings[-1]) * 100, 2) for time in timings]
+
+            timings_str = [
+                f'{timings[i]} ({perc[i]}\%)' for i in range(len(timings))]
+
+            record += record_list[1: 2] + record_list[8:14] + \
+                [str(timings[-1])] + timings_str[:2]
+
+            record = "& ".join(record)
+            latex_table.append(record)
+
+    for line in latex_table:
+        print(f'{line}\\\\')
+
+
+if __name__ == '__main__':
+    main()
