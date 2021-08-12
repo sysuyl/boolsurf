80d7bf050be548b6da48f0fb6b5663b6f051b34c
Marzia Riso <riso@di.uniroma1.it>
2021-08-12 16:30:10 +0200

    Fix jsons

--- apps/test/clean-test.py
+++ apps/test/clean-test.py
@@ -12,7 +12,7 @@ def clean(dirname):
             js = json.load(infile)
 
         model = js['model']
-        model = model.replace(".", "")
+        model = model.replace("./", "")
         model = model.replace("\\", "/")
         model = model.replace("//", "/")
         js['model'] = model
@@ -21,6 +21,23 @@ def clean(dirname):
             json.dump(js, outfile, indent=2)
 
 
+def riclean(dirname):
+    files = glob.glob(f"{dirname}/*.json")
+    print(len(files))
+
+    for file in files:
+        with open(file) as infile:
+            js = json.load(infile)
+
+        model = js['model']
+        model = model[:-3]
+        model += ".ply"
+        js['model'] = model
+
+        with open(file, 'w') as outfile:
+            json.dump(js, outfile, indent=2)
+
+
 if __name__ == "__main__":
     dirname = sys.argv[1]
-    clean(dirname)
+    riclean(dirname)
