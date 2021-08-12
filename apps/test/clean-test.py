a98098a7f6726b920c64b0180e913ac6e7654caa
Marzia Riso <riso@di.uniroma1.it>
2021-08-12 16:24:55 +0200

    Added json fix

---
+++ apps/test/clean-test.py
@@ -0,0 +1,26 @@
+import glob
+import json
+import sys
+
+
+def clean(dirname):
+    files = glob.glob(f"{dirname}/*.json")
+    print(len(files))
+
+    for file in files:
+        with open(file) as infile:
+            js = json.load(infile)
+
+        model = js['model']
+        model = model.replace(".", "")
+        model = model.replace("\\", "/")
+        model = model.replace("//", "/")
+        js['model'] = model
+
+        with open(file, 'w') as outfile:
+            json.dump(js, outfile, indent=2)
+
+
+if __name__ == "__main__":
+    dirname = sys.argv[1]
+    clean(dirname)
